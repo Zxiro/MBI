6c8a0f47777f2bed0c1390fb887bab88082ce554
zxiro <a0963212510@gmail.com>
2019-12-05 15:16:18 +0800

    Modified the CsvtoTrainingData to be able to Travesl all csv files

--- stock_info.py
+++ stock_info.py
@@ -1,3 +1,4 @@
+%load StockInfo.ipynb
 import datetime
 from datetime import timedelta
 import json
@@ -13,11 +14,15 @@ dirPath = r"/home/db/stock_resource_center/resource/twse/json"
 #dirPath = r"./stock_data"
 result = [f for f in sorted(os.listdir(dirPath)) if os.path.isfile(os.path.join(dirPath, f))]
 
-for i in result:
+for i in result
     data_locate = os.path.join(dirPath,i)
-    data = pd.read_json(data_locate)
-    data = data.T
-    data = data.loc[[stock_symbol],['open','high','low','close','volume']]
+    #data = pd.read_json(data_locate)
+    with open(data_locate, 'r') as f:
+        data = json.load(f)
+    data = data.get(stock_symbol)
+    data = pd.DataFrame.from_dict(data,orient='index').T
+    #data = data.T
+    #data = data.loc[[stock_symbol],['open','high','low','close','volume']]
     column = data.columns.tolist()
     column.insert(0,"date")
     data = data.reindex(columns = column)
@@ -27,30 +32,7 @@ for i in result:
         stock_data = data
     else:
         stock_data = pd.concat([stock_data,data],axis=0)
-'''
-for i in last_date:
-    data_locate = "./stock_data/"+i+".json"
-    data = pd.read_json(data_locate)
-    data = data.T
-    data = data.loc[[stock_symbol],['high','low','open','close']]
-    column = data.columns.tolist()
-    column.insert(0,"date")
-    data = data.reindex(columns = column)
-    data["date"] = i;
-    print(data)
-    if i == last_date[0]:
-        stock_data = data
-    else:
-        stock_data = pd.concat([stock_data,data],axis=0)
-'''
-'''
-date = "2019-09-02"
-data_locate = "./stock_data/"+date+".json"
-data = pd.read_json(data_locate)
-data = data.T
-print(data.loc[[stock_symbol],['high','low','open','close']])
-stock_data = data.loc[[stock_symbol],['high','low','open','close']]
-'''
+
 print(stock_data)
-file_name = "stock_"+stock_symbol+".csv"
+file_name = "stock"+stock_symbol+".csv"
 stock_data.to_csv(file_name,index=False)
