a597ab48a0f213cea39748c79589cbe77805dbdc
Hsinmin <andy690100391@gmail.com>
2020-02-25 21:38:53 +0800

    can call class evaluate

---
+++ call_evaluate.py
@@ -0,0 +1,11 @@
+import os
+import sys
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]=""
+from evaluate import Evaluate
+if len(sys.argv) < 2 :
+    stock_symbol = input('輸入股票號碼:')
+else:
+    stock_symbol = sys.argv[1]
+evaluate = Evaluate(stock_symbol)
+evaluate.roi()
