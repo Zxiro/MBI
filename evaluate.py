e17dab963efbeb37564c515da4e3c509fd070196
Hsinmin <andy690100391@gmail.com>
2020-02-25 21:37:59 +0800

    evaluate can input stock number

--- evaluate.py
+++ evaluate.py
@@ -1,22 +1,30 @@
 class Evaluate:
     def __init__(self,stock):
+        import numpy as np
+        import pandas as pd
+        #import tensorflow as tf
+        #from tensorflow.keras.models import load_model
+        from keras.models import load_model
+
         '''引入資料'''
-        x_test = np.load('./StockData/TrainingData/NormtestingX_stock'+stock+'.npy')
-        y_test = np.load('./StockData/TrainingData/testingY_stock'+stock+'.npy')
-        origin_x_test = np.load('./StockData/TrainingData/opentestingX_stock'+stock+'.npy') #每個禮拜一的開盤價
-        model = load_model('./stockmodel/stockmodel_'+stock+'.h5') #引入訓練完model
-    def roi():
+        self.x_test = np.load('./StockData/TrainingData/NormtestingX_stock'+stock+'.npy')
+        self.y_test = np.load('./StockData/TrainingData/testingY_stock'+stock+'.npy')
+        self.origin_x_test = np.load('./StockData/TrainingData/opentestingX_stock'+stock+'.npy') #每個禮拜一的開盤價
+        self.model = load_model('./stockModel/stockmodel_'+stock+'.h5') #引入訓練完model
+        self.x_test = self.x_test.reshape(-1,5,self.x_test.shape[1])
+    def roi(self):
         '''
         利用預測的數字如果為正，依照預測的數字比例來進行購買股票以1000為基準乘以(1+預測數字)
         而realmoney則為利用y_test的值去判斷是否依照比例進行購買
         '''
+        #print(self.x_test)
         get_from_predict = 0   #預測所能獲得金額
         predict_cost = 0 #成本
         real_cost = 0
         get_from_real = 0   #真實獲得金額
         amount = 1000  #購買的數量
-        predict = model.predict( x_test )
-        for predict_money , real_money , open_money in zip( predict , y_test ,origin_x_test):
+        predict = self.model.predict( self.x_test )
+        for predict_money , real_money , open_money in zip( predict , self.y_test ,self.origin_x_test):
             if predict_money > 0:
                 get_from_predict += int( amount ) * real_money
                 predict_cost += open_money * int( amount )
@@ -30,9 +38,8 @@ class Evaluate:
         print('predict cost' , predict_cost)
         print('real money:' , get_from_real)
         money = round((( get_from_predict/predict_cost )*100) , 2)
-        print(money , '%')
+        print('roi with predict:',money , '%')
         money = round((( get_from_real/real_cost )*100) , 2)
-        print(money , '%')
-
+        print('roi with answer:',money , '%')
 
 
