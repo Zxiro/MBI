0dc0d5ff6f41b201ab6809f9828c286b9be903f4
zxiro <a0963212510@gmail.com>
2020-03-28 16:43:19 +0800

    finish adding usa index and tunred into training data

--- build_train_data.py
+++ build_train_data.py
@@ -54,48 +54,52 @@ def resha(x): #從 (幾周,每周幾天,特徵數)reshape成(天*周,特徵數) 
     nptrain = np.reshape(nptrain,(nptrain.shape[0]*nptrain.shape[1], nptrain.shape[2]))
     return nptrain
 
-def save_np(x,y,open_money):
-    path = './StockData/stock0050.csv'
+def save_np(x,y,open_money,num):
+    path = './StockData/'+num+'csv'
     #train_x, x_test,train_y, y_test = train_test_split(x,y,test_size=0.25,random_state=42)
     train_x = x[:-50]
     train_y = y[:-50]
     x_test = x[-50:]
     y_test = y[-50:]
     open_money = open_money[-50:]
-    stock_name = path[12:21]
+    stock_name = num
     scaler = preprocessing.StandardScaler() #初始化scaler
     scaler = scaler.fit(resha(train_x))  #  標準化後的數據
     train_x = scaler.transform(resha(train_x))
     Npdata = train_x
     np.save(os.path.join('./StockData/TrainingData/', 'NormtrainingX_' + stock_name), Npdata)
-    print(path[12:21] ," trainX  ", Npdata.shape)
+    print(num ," trainX  ", Npdata.shape)
     print(Npdata)
     Npdata = scaler.transform(resha(x_test))# normalize x_test with scale of train_x
     np.save(os.path.join('./StockData/TrainingData/', 'NormtestingX_' + stock_name), Npdata)
-    print(path[12:21], " testX  ", Npdata.shape) 
+    print(num, " testX  ", Npdata.shape) 
     print(Npdata)
     Npdata = np.array(train_y)
     np.save(os.path.join('./StockData/TrainingData/', 'trainingY_' + stock_name), Npdata)
-    print(path[12:21], " trainY  ", Npdata.shape) 
+    print(num, " trainY  ", Npdata.shape) 
     print(Npdata)
     Npdata = np.array(y_test)
     np.save(os.path.join('./StockData/TrainingData/', 'testingY_' + stock_name), Npdata)
-    print(path[12:21], " testY  ", Npdata.shape) 
+    print(num, " testY  ", Npdata.shape) 
     print(Npdata)
     Npdata = np.array(open_money)
     np.save(os.path.join('./StockData/TrainingData/', 'opentestingX_' + stock_name), Npdata)
-    print(path[12:21], " opentestX  ", Npdata.shape) 
+    print(num, " opentestX  ", Npdata.shape) 
     print(Npdata)
 
-def generate_train(close_type, feature, data):
+def generate_train(close_type, feature, data, usanum, name):
     train_x = []
     train_y = []
+    tmp = []
     open_money = []
     for _, span_data in data:
-        span_data = span_data.dropna()
         if len(span_data) == 5: #Decide the way seperate the stock data
-            xlist = span_data.loc[:, feature].values.tolist()
-            train_x.append(xlist) 
+            #print(span_data)
+            index = span_data.iloc[ :,-5*usanum:].values.tolist()#usa index
+            xlist = span_data.loc[:, feature].values.tolist()#select feature
+            for i in range(0,5):
+                tmp.append(xlist[i]+index[i])
+            train_x.append(tmp)
             mon_open = span_data['open'][0]
             fri_close = span_data['close'][4]
             open_money.append(((mon_open)))
@@ -104,44 +108,43 @@ def generate_train(close_type, feature, data):
             continue
     #print(train_x)
     #print(train_y)
+    save_np(train_x,train_y,open_money, name)
 
-    save_np(train_x,train_y,open_money)
-
-def add_features(csv_data):
-    csv_data['rsv'] = add_rsv(csv_data)
-    csv_data['k'] = add_k(csv_data)
-    csv_data['d'] = add_d(csv_data)
-    add_MA(csv_data)
-    csv_data = csv_data.set_index('date').resample('w')
-    return csv_data
+def add_features(df):
+    df['rsv'] = add_rsv(df)
+    df['k'] = add_k(df)
+    df['d'] = add_d(df)
+    add_MA(df)
     
 
-def load_csv():
-    stock_data = pd.DataFrame(pd.read_csv('./StockData/stock0050.csv'))
+def load_csv(num):
+    stock_data = pd.DataFrame(pd.read_csv('./StockData/'+num+'.csv'))
     stock_data['date'] = pd.to_datetime(stock_data['date'])
     index_list = glob.glob(r"./usa_stock_data/*.csv")
     for i in range(0,4):
         index_list[i] = pd.DataFrame(pd.read_csv(index_list[i]))
         index_list[i] = index_list[i].drop(index_list[i].columns[0], axis=1)
-    stock_data = stock_data.drop([0], axis=0) #drop 第一天 因為stockdata 有16年跳到17年的問題
+    stock_data = stock_data.drop([0], axis=0)  #drop 第一天 因為stockdata 有16年跳到17年的問題
     stock_data = stock_data.reset_index(drop=True)
-    index_list.insert(0,stock_data)
-    return stock_data, index_list
+    index_list.insert(0, stock_data)
+    return index_list
 
 with open("./config.json",'r') as load_f:
     config = json.load(load_f)
-
+stock_num = config['stock_num']
 feature = config['features']
-print("features: " , feature)
 span = config['span']
-print("span: " , span)
 close_type = config['close_type']
-print("close_type: " , close_type)
+usa_index = config['usa_index'] #Unwanted USA index
 
-df, USA = load_csv() #df 股票資料  USA 股票加上USA index
-re = pd.concat(USA, axis=1).reindex(USA[0].index)
-print(re)
-re.to_csv('/home/zxiro/MBI/feature/re.csv')
-exit()
-df = add_features(df)
-generate_train(close_type,feature,df)
+USA = load_csv(stock_num) #df 股票資料  USA 股票加上USA index
+add_features(USA[0])
+df = pd.concat(USA, axis=1).reindex(USA[0].index) 
+for index in usa_index:
+    tmp = df.columns.str.contains(index)
+    df = df[df.columns[~tmp]] #丟掉不需要的usa index
+df = df.dropna()
+print(df)
+df.to_csv('/home/zxiro/MBI/feature/df.csv')
+df = df.set_index('date').resample('w')
+generate_train(close_type, feature, df, len(usa_index),stock_num)
