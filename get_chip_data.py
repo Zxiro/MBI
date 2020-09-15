8318a3264b2fc7169b5ca33c05a292ab063e91ca
zxiro <a0963212510@gmail.com>
2020-09-15 21:43:09 +0800

    Modify the df column name and drop useless column

--- get_chip_data.py
+++ get_chip_data.py
@@ -16,29 +16,23 @@ for i in stock_data["date"]:
         stock_data.drop([count], axis = 0, inplace = True)
     count = count + 1
 stock_data = stock_data.reset_index(drop = True)
-stock_dic['week_start'] = stock_data['date']
 
-for i in range(len(stock_dic['week_start'])):
-    week_start = str(stock_dic['week_start'][i]).replace("-", "")
+for i in range(len(stock_data['date'])):
+    week_start = str(stock_data['date'][i]).replace("-", "")
     if(len(week_start)!=0):
-        stock_dic['week_start'][i] = week_start[:8]
-stock_date = stock_dic['week_start']
+       stock_data['date'][i] = week_start[:8]
 
-for i in range(len(stock_date)):
-    print(stock_date[i])
-    tse_csv = requests.get('https://www.twse.com.tw/fund/T86?response=csv&date='+stock_date[i]+'&selectType=ALLBUT0999')
+for i in range(len(stock_data['date'])):
+    print(stock_data['date'][i])
+    tse_csv = requests.get('https://www.twse.com.tw/fund/T86?response=csv&date='+stock_data['date'][i]+'&selectType=ALLBUT0999')
     df = pd.read_csv(StringIO(tse_csv.text), header = 1).dropna(how='all', axis=1).dropna(how='any')
     df['stock_id'] = df['證券代號'].str.replace('=','').str.replace('"','')
     df['inv_trust'] = df['投信買賣超股數']
     df['dealer'] = df['自營商買賣超股數']
-    df['institutional_inv_overview'] = df['三大法人買賣超股數']
-    df = df.drop(['投信買賣超股數'], axis=1)
-    df = df.drop(['證券代號'], axis=1)
-    df = df.drop(['自營商買賣超股數'], axis=1)
-    df = df.drop(['證券名稱'], axis=1)
-    df = df.drop(['三大法人買賣超股數'], axis=1)
+    df['institution_inv_overview'] = df['三大法人買賣超股數']
+    df['foregin_inv'] = df['institution_inv_overview'] - df['inv_trust'] - df['dealer']
+    df.drop(df.columns[ :-5],inplace = True, axis = 1)
     mask = (df['stock_id'] == stock_id)
     print(df[mask])
-    time.sleep(3)
-    print("GOGONEXT")
+    time.sleep(10)
 
